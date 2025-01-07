a2b1c98cc614fc2cdf557f07f695832ce4f73c91
Jwles <jules.magnan@epita.fr>
2025-01-07 15:59:45 +0100

    feat: -c

--- src/42sh.c
+++ src/42sh.c
@@ -1,8 +1,43 @@
+#include <err.h>
+#include <getopt.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "ast/ast.h"
+#include "utils/fill_input.h"
 #include "utils/logger.h"
 
-int main(void)
+static struct option l_opts[] = { { "verbose", no_argument, 0, 'v' },
+                                  { "comput", no_argument, 0, 'c' },
+                                  { 0, 0, 0, 0 } };
+
+int main(int argc, char *argv[])
 {
-    print_ast();
+    char *input = calloc(1, sizeof(char));
+    int c;
+    int opt_idx = 0;
+    while ((c = getopt_long(argc, argv, "v::c:", l_opts, &opt_idx)) != -1)
+    {
+        switch (c)
+        {
+        case 'v':
+            logger("Test %s\n", "hello");
+            logger(NULL, NULL);
+            logger("Test %s\n", "hello");
+            break;
+        case 'c':
+            fill_input_with_argv(argv, argc, optind, input);
+            logger("%s", input);
+            break;
+        case '?':
+            exit(1);
+
+        default:
+            errx(1, "main: unkown option %c", c);
+        }
+    }
+
     return 0;
 }
