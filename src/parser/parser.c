4e9aec2fb6c7d5ac0ff88189fc278c578e1ee812
Jwles <jules.magnan@epita.fr>
2025-01-08 16:05:47 +0100

    feat: ast

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,10 +1,25 @@
 #include "parser.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "ast/ast.h"
 #include "lexer/lexer.h"
+#include "lexer/token.h"
+#include "utils/logger.h"
 
-void print_parser()
+struct ast_node *parse(__attribute__((unused)) struct lexer *lexer)
 {
-    printf("parser !!!\n");
+    return NULL;
+}
+
+struct ast_node *parse_expr(__attribute__((unused)) struct lexer *lexer)
+{
+    return NULL;
+}
+
+struct ast_node *
+parse_simple_command(__attribute__((unused)) struct lexer *lexer)
+{
+    return NULL;
 }
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,6 +1,8 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-void print_parser();
-
+#include "lexer/lexer.h"
+struct ast_node *parse_simple_command(struct lexer *lexer);
+struct ast_node *parse_expr(struct lexer *lexer);
+struct ast_node *parse(struct lexer *lexer);
 #endif /* ! PARSER_H */
