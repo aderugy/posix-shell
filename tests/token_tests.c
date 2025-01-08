b467aaa3dd9c41f2ff6ea18533b5910b82fe95ec
Jwles <jules.magnan@epita.fr>
2025-01-08 13:25:13 +0100

    test: feat

--- tests/token_tests.c
+++ tests/token_tests.c
@@ -1,4 +1,72 @@
-int main(void)
+#include <err.h>
+#include <getopt.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "lexer/splitter.h"
+#include "streams/streams.h"
+#include "utils/logger.h"
+
+static struct option l_opts[] = { { "verbose", no_argument, 0, 'v' },
+                                  { "comput", required_argument, 0, 'c' },
+                                  { "test-token", no_argument, 0, 't' },
+
+                                  { 0, 0, 0, 0 } };
+
+int main(int argc, char *argv[])
 {
-    return 1;
+    int c;
+    int opt_idx = 0;
+    struct stream *stream = NULL;
+    while ((c = getopt_long(argc, argv, "vc:t", l_opts, &opt_idx)) != -1)
+    {
+        switch (c)
+        {
+        case 'v':
+            logger(NULL, NULL);
+            logger("--verbose is activated\n");
+            break;
+        case 'c':
+            stream = stream_from_str(optarg);
+            break;
+        case 't':
+            break;
+        case '?':
+            exit(1);
+
+        default:
+            errx(1, "main: unkown option %c", c);
+        }
+    }
+
+    if (!stream)
+    {
+        if (argc > 1)
+        {
+            char *path = argv[1];
+            stream = stream_from_file(path);
+        }
+        else
+        {
+            stream = stream_from_stream(stdin);
+        }
+    }
+
+    if (!stream)
+    {
+        errx(1, "stream error");
+    }
+
+    struct shard *shard;
+    while ((shard = splitter_next(stream)))
+    {
+        printf("%s (%d)\n", shard->data, shard->quoted);
+        free(shard->data);
+        free(shard);
+    }
+
+    stream_close(stream);
+    return 0;
 }
