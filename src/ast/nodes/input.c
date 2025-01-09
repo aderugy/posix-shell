2fc82809b79697f138cb53ac73a6b4dd62c6465f
gaspard.torterat-slanda <gaspard.torterat-slanda@epita.fr>
2025-01-09 15:08:18 +0100

    feat(ast/input): parser

--- src/ast/nodes/input.c
+++ src/ast/nodes/input.c
@@ -3,9 +3,28 @@
 #include <err.h>
 #include <stdlib.h>
 
-struct ast_input *ast_parse_input(__attribute((unused)) struct lexer *lexer)
+struct ast_input *ast_parse_input(struct lexer *lexer)
 {
-    errx(EXIT_FAILURE, "not implemented");
+    struct token tok = lexer_peek(lexer);
+    struct ast_list *root = NULL;
+    switch (tok.type)
+    {
+    case TOKEN_EOF:
+    case TOKEN_NEW_LINE:
+        return NULL;
+    default:
+        tok = lexer_pop(lexer);
+        root = parse_list(lexer);
+        // if root == NULL then program has already exited
+        // no need to exit
+        break;
+    }
+    tok = lexer_pop(lexer);
+    if (tok.type != TOKEN_EOF && tok.type != TOKEN_NEW_LINE)
+    {
+        return errx("Unexpected token at the end of input parsing");
+    }
+    return root;
 }
 int ast_eval_input(__attribute((unused)) struct ast_input *node,
                    __attribute((unused)) void **out)
