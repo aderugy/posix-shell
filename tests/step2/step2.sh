b1c9cdb2dae0316517e691b8116616a798742cd9
mehdi.azouz <mehdi.azouz@epita.fr>
2025-01-10 22:50:23 +0100

    add: some test

--- tests/step2/step2.sh
+++ tests/step2/step2.sh
@@ -27,46 +27,45 @@ D="\033[00m"
 # @brief runs a test on all possible input ways
 # @params: a list of strings
 tes() {
-    echo "$@" > "$SCRIPT"
-    test_from_direct_input "$@"
-    test_from_file "$SCRIPT"
-    test_from_stdin "$SCRIPT"
+  echo "$@" >"$SCRIPT"
+  test_from_direct_input "$@"
+  test_from_file "$SCRIPT"
+  test_from_stdin "$SCRIPT"
 }
 
 # @params: a list of files
 test_from_stdin() {
-    bash --posix < "$@" > "$EXPECTED_OUT" 2> "$EXPECTED_ERR_OUT"
-    "$F" < "$@" > "$ACTUAL_OUT" 2> "$ACTUAL_ERR_OUT"
-    output_test "./$BIN < $@"
+  bash --posix <"$@" >"$EXPECTED_OUT" 2>"$EXPECTED_ERR_OUT"
+  "$F" <"$@" >"$ACTUAL_OUT" 2>"$ACTUAL_ERR_OUT"
+  output_test "./$BIN < $@"
 }
 
 # @params: a list of files
 test_from_file() {
-    bash --posix "$@" > "$EXPECTED_OUT" 2> "$EXPECTED_ERR_OUT"
-    "$F" "$@" > "$ACTUAL_OUT" 2> "$ACTUAL_ERR_OUT"
-    output_test "./$BIN" "$@"
+  bash --posix "$@" >"$EXPECTED_OUT" 2>"$EXPECTED_ERR_OUT"
+  "$F" "$@" >"$ACTUAL_OUT" 2>"$ACTUAL_ERR_OUT"
+  output_test "./$BIN" "$@"
 }
 
 # @params: a list of strings
 test_from_direct_input() {
-    bash --posix -c "$@" > "$EXPECTED_OUT" 2> "$EXPECTED_ERR_OUT"
-    "$F" -c "$@" > "$ACTUAL_OUT" 2> "$ACTUAL_ERR_OUT"
-    output_test "./$BIN -c" "$@"
+  bash --posix -c "$@" >"$EXPECTED_OUT" 2>"$EXPECTED_ERR_OUT"
+  "$F" -c "$@" >"$ACTUAL_OUT" 2>"$ACTUAL_ERR_OUT"
+  output_test "./$BIN -c" "$@"
 }
 
 # @params: err code and then a list of strings
-test_code_error()
-{
-    ERR="$1"
-    shift
-    "$F" -c "$@"
-    ACTUAL_ERR="$?"
-    if [ $ACTUAL_ERR -eq $ERR ]; then
-        echo "$G[OK]$D $BIN -c \""$@"\"$D"
-    else
-        echo "COMMAND RUN : $R$F -c \""$@"\"$D"
-        echo "EXPECTED $G$ERR$D. GOT $R$ACTUAL_ERR$D"
-    fi
+test_code_error() {
+  ERR="$1"
+  shift
+  "$F" -c "$@"
+  ACTUAL_ERR="$?"
+  if [ $ACTUAL_ERR -eq $ERR ]; then
+    echo "$G[OK]$D $BIN -c \""$@"\"$D"
+  else
+    echo "COMMAND RUN : $R$F -c \""$@"\"$D"
+    echo "EXPECTED $G$ERR$D. GOT $R$ACTUAL_ERR$D"
+  fi
 }
 
 # @brief output the results
@@ -74,125 +73,142 @@ test_code_error()
 # @params the command that was run
 # @remark change the options of diff as u like eg. try -y (column), -u, -q
 output_test() {
-    FA=$1
-    shift
-    diff -y --color="always" "$EXPECTED_OUT" "$ACTUAL_OUT" > "$DIFF_OUT" 2>&1
+  FA=$1
+  shift
+  diff -y --color="always" "$EXPECTED_OUT" "$ACTUAL_OUT" >"$DIFF_OUT" 2>&1
+  if [ $? -eq 0 ]; then
+    diff -y --color="always" "$EXPECTED_ERR_OUT" "$ACTUAL_ERR_OUT" >"$DIFF_ERR" 2>&1
     if [ $? -eq 0 ]; then
-        diff -y --color="always" "$EXPECTED_ERR_OUT" "$ACTUAL_ERR_OUT" > "$DIFF_ERR" 2>&1
-        if [ $? -eq 0 ]; then
-            echo "$G[OK]$D $FA \""$@"\""
-        else
-            echo "$R[KO]$D"
-            echo "$FA \""$@"\""
-            E=$(cat -e "$EXPECTED_ERR")
-            A=$(cat -e "$ACTUAL_ERR")
-            echo "$G$E$D"
-            echo "$R$A$D"
-        fi
+      echo "$G[OK]$D $FA \""$@"\""
     else
-            echo "$R[KO]$D"
-            echo "$FA \""$@"\""
-            E=$(cat -e "$EXPECTED_OUT")
-            A=$(cat -e "$ACTUAL_OUT")
-            echo "$G$E$D"
-            echo "$R$A$D"
+      echo "$R[KO]$D"
+      echo "$FA \""$@"\""
+      E=$(cat -e "$EXPECTED_ERR")
+      A=$(cat -e "$ACTUAL_ERR")
+      echo "$G$E$D"
+      echo "$R$A$D"
     fi
+  else
+    echo "$R[KO]$D"
+    echo "$FA \""$@"\""
+    E=$(cat -e "$EXPECTED_OUT")
+    A=$(cat -e "$ACTUAL_OUT")
+    echo "$G$E$D"
+    echo "$R$A$D"
+  fi
 }
-test_pipeline()
-{
-    echo "========== PIPELINE BEGIN =========="
-    tes "echo Hello | tr a e"
-    tes "echo Hello | tr a e | tr e a"
-    tes "echo Hello | tr a e | tr e a | tr a e"
-    tes "find -name *.c | echo"
-    echo "========== PIPELINE END =========="
+test_pipeline() {
+  echo "========== PIPELINE BEGIN =========="
+  tes "echo Hello | tr a e"
+  tes "echo Hello | tr a e | tr e a"
+  tes "echo Hello | tr a e | tr e a | tr a e"
+  tes "find -name *.c | echo"
+  echo "========== PIPELINE END =========="
 }
-test_neg_pipeline()
-{
-    echo "========== NEGATION PIPELINE BEGIN =========="
-    test_code_error 0 "! true | false"
-    test_code_error 1 "! false | true"
-    test_code_error 0 "! false | true | false | false | false | false"
-    echo "========== NEGATION PIPELINE END =========="
+test_neg_pipeline() {
+  echo "========== NEGATION PIPELINE BEGIN =========="
+  test_code_error 0 "! true | false"
+  test_code_error 1 "! false | true"
+  test_code_error 0 "! false | true | false | false | false | false"
+  echo "========== NEGATION PIPELINE END =========="
 }
-test_ops()
-{
-    echo "========== OPS BEGIN =========="
-    tes "true && false && false || echo a"
-    tes "true && echo b && true || echo a"
-    tes "true && ls && echo b || echo a"
-    tes "true && false && echo b || echo a"
-    tes "false && false && echo b || echo a"
-    tes "false && echo b || echo a && true && false || true"
-    tes "echo a && false || echo h"
-    echo "========== OPS END =========="
+test_ops() {
+  echo "========== OPS BEGIN =========="
+  tes "true && false && false || echo a"
+  tes "true && echo b && true || echo a"
+  tes "true && ls && echo b || echo a"
+  tes "true && false && echo b || echo a"
+  tes "false && false && echo b || echo a"
+  tes "false && echo b || echo a && true && false || true"
+  tes "echo a && false || echo h"
+  echo "========== OPS END =========="
 }
-test_var()
-{
-    for i in $(find tests/step2/assignement_substitution -name "*sh");
-    do
-    test_from_file $i;
-    test_from_stdin $i;
-    done;
+test_for() {
+  echo "========== FOR BEGIN =========="
+  tes "for i in 1 2 3; do echo \"Value: \$i\"; done"
+  tes "for file in \$(ls); do echo \"File: \$file\"; done"
+  tes "for i in \$(seq 1 5); do echo \"Number: \$i\"; done"
+  tes "for cmd in ls nonexistentcommand pwd; do \$cmd || echo \"Error with \$cmd\"; done"
+  tes "for i in 1 2; do for j in a b; do echo \"\$i\$j\"; done; done"
+  echo "========== FOR END =========="
 }
-test_non_builtin()
-{
-    echo "========== NON_BUILTIN BEGIN =========="
-    tes "ls -a"
-    tes "tree -L 2"
-    tes "find -name *.c"
-    echo "========== NON_BUILTIN END =========="
+
+test_while_loops() {
+  echo "========== WHILE LOOP BEGIN =========="
+  tes "i=1; while [ \$i -le 5 ]; do echo \"Number: \$i\"; i=\$((i + 1)); done"
+  tes "while read -r line; do echo \"Line: \$line\"; done < <(echo -e \"first\\nsecond\\nthird\")"
+  tes "i=0; while [ \$i -lt 3 ]; do echo \"Iteration \$i\"; i=\$((i + 1)); done"
+  tes "i=1; while [ \$i -le 3 ]; do [ \$i -eq 2 ] && false || echo \"Step \$i\"; i=\$((i + 1)); done"
+  tes "i=1; while [ \$i -le 2 ]; do j=1; while [ \$j -le 2 ]; do echo \"\$i\$j\"; j=\$((j + 1)); done; i=\$((i + 1)); done"
+  echo "========== WHILE LOOP END =========="
 }
-test_comment()
-{
-    echo "========== COMMENT BEGIN =========="
-    tes echo "Ya un commentaire m c chill #de ouf c chill"
-    tes echo "Ya un commentaire m c chill \# mais moi je suis pas un comment"
-    tes 'echo \escaped \#escaped "#"quoted not#first #commented'
-    echo "========== COMMENT END =========="
+test_var() {
+  for i in $(find tests/step2/assignement_substitution -name "*sh"); do
+    test_from_file $i
+    test_from_stdin $i
+  done
 }
-test_mix()
-{
-    echo "========== MIX BEGIN =========="
-    tes 'echo -e \escaped \#escaped "#"quoted not#first #commented'
-    echo
-    tes 'if true; then echo -Ee "\escaped \#escaped "#"quoted not#first
-    #commented"; fi'
-    echo "========== MIX END =========="
+test_non_builtin() {
+  echo "========== NON_BUILTIN BEGIN =========="
+  tes "ls -a"
+  tes "tree -L 2"
+  tes "find -name *.c"
+  echo "========== NON_BUILTIN END =========="
 }
-test_errs()
-{
-    echo "========== ERROR_CODE BEGIN =========="
-    # PARSE ERRS
-    test_code_error 2 "false | | true"
-    test_code_error 2 "| false || true"
-    test_code_error 2 "false | true |"
-    test_code_error 2 "|| false"
-    test_code_error 2 "|| false"
-    test_code_error 2 "|| false"
-    test_code_error 2 "|| true &&"
-    test_code_error 2 "false || &&"
-    test_code_error 2 "true && true && false || || true"
-    test_code_error 2 "echo | | |"
-    # PIPELINE ERRS
-    test_code_error 1 "true | false"
-    test_code_error 0 "false | true"
-    test_code_error 1 "false | true | false | false | false | false"
-    # LEXER ERRS
-    test_code_error 2 "if true; then echo a; \"fi"
-    test_code_error 2 "\""
-    test_code_error 2 "\"\"\""
-    echo "========== ERROR_CODE END =========="
+test_comment() {
+  echo "========== COMMENT BEGIN =========="
+  tes echo "Ya un commentaire m c chill #de ouf c chill"
+  tes echo "Ya un commentaire m c chill \# mais moi je suis pas un comment"
+  tes 'echo \escaped \#escaped "#"quoted not#first #commented'
+  echo "========== COMMENT END =========="
 }
-testsuite()
-{
-   test_pipeline
-   test_neg_pipeline
-   test_ops
-   test_var
-   test_comment
-   test_mix
-   test_errs
+test_mix_grammar() {
+  echo "========== MIXED GRAMMAR TESTS BEGIN =========="
+  tes "for i in 1 2 3; do if [ \$((i % 2)) -eq 0 ]; then echo \"Even: \$i\" | tr a-z A-Z; else echo \"Odd: \$i\"; fi; done"
+  tes "for i in 1 2; do j=1; while [ \$j -le \$i ]; do echo \"\$i,\$j\" >> output.txt; j=\$((j + 1)); done; done; cat output.txt; rm output.txt"
+  tes "i=1; while [ \$i -le 3 ]; do for j in a b; do if [ \$i -eq 2 ] && [ \"\$j\" = \"b\" ]; then echo \"Special case: \$i\$j\"; else echo \"Regular case: \$i\$j\"; fi; done; i=\$((i + 1)); done"
+  tes "i=1; while [ \$i -le 3 ]; do case \$i in 1) echo \"First\" ;; 2) echo \"Second\" ;; 3) echo \"Third\" ;; esac; i=\$((i + 1)); done"
+  tes "for word in \$(echo \"one two three\" | tr ' ' '\n'); do echo \"Word: \$word\" | rev; done"
+  tes "for i in 1 2; do echo \"Start \$i\"; while [ \$i -le 2 ]; do echo \"Inside \$i\"; i=\$((i + 1)); done; echo \"End \$i\"; done"
+  tes "for i in 1 2; do while [ \$i -le 2 ]; do if [ \$i -eq 2 ]; then false || echo \"Error on \$i\"; else echo \"OK: \$i\"; fi; break; done; done"
+  tes "for i in 1 2; do j=1; while [ \$j -le 2 ]; do if [ \$((i * j)) -gt 2 ]; then echo \"\$i * \$j = \$((i * j))\" >> output.txt; else echo \"Below threshold: \$i \$j\"; fi; j=\$((j + 1)); done; done; cat output.txt; rm output.txt"
+  echo "========== MIXED GRAMMAR TESTS END =========="
+}
+test_errs() {
+  echo "========== ERROR_CODE BEGIN =========="
+  # PARSE ERRS
+  test_code_error 2 "false | | true"
+  test_code_error 2 "| false || true"
+  test_code_error 2 "false | true |"
+  test_code_error 2 "|| false"
+  test_code_error 2 "|| false"
+  test_code_error 2 "|| false"
+  test_code_error 2 "|| true &&"
+  test_code_error 2 "false || &&"
+  test_code_error 2 "true && true && false || || true"
+  test_code_error 2 "echo | | |"
+  # PIPELINE ERRS
+  test_code_error 1 "true | false"
+  test_code_error 0 "false | true"
+  test_code_error 1 "false | true | false | false | false | false"
+  # LEXER ERRS
+  test_code_error 2 "if true; then echo a; \"fi"
+  test_code_error 2 "\""
+  test_code_error 2 "\"\"\""
+  test_code_error 2 "for test"
+  test_code_error 2 "while test"
+  echo "========== ERROR_CODE END =========="
+}
+testsuite() {
+  test_pipeline
+  test_neg_pipeline
+  test_ops
+  test_var
+  test_comment
+  test_for
+  test_while_loops
+  test_mix_grammar
+  test_errs
 }
 
 testsuite
