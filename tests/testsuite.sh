70976ac33c3fc6e20c7dfb020abc8cc670ef09bc
Jwles <jules.magnan@epita.fr>
2025-01-10 14:20:13 +0100

    2

--- tests/testsuite.sh
+++ tests/testsuite.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # for storing stdout
 EXPECTED_OUT=".original_fun.out"
 ACTUAL_OUT=".my_fun.out"
@@ -27,7 +26,7 @@ D="\033[00m"
 # @brief runs a test on all possible input ways
 # @params: a list of strings
 tes() {
-    echo "$@" > "$SCRIPT"
+    echo "$@" >"$SCRIPT"
     test_from_direct_input "$@"
     test_from_file "$SCRIPT"
     test_from_stdin "$SCRIPT"
@@ -35,28 +34,27 @@ tes() {
 
 # @params: a list of files
 test_from_stdin() {
-    bash --posix < "$@" > "$EXPECTED_OUT" 2> "$EXPECTED_ERR_OUT"
-    "$F" < "$@" > "$ACTUAL_OUT" 2> "$ACTUAL_ERR_OUT"
+    bash --posix <"$@" >"$EXPECTED_OUT" 2>"$EXPECTED_ERR_OUT"
+    "$F" <"$@" >"$ACTUAL_OUT" 2>"$ACTUAL_ERR_OUT"
     output_test "./$BIN < $@"
 }
 
 # @params: a list of files
 test_from_file() {
-    bash --posix "$@" > "$EXPECTED_OUT" 2> "$EXPECTED_ERR_OUT"
-    "$F" "$@" > "$ACTUAL_OUT" 2> "$ACTUAL_ERR_OUT"
+    bash --posix "$@" >"$EXPECTED_OUT" 2>"$EXPECTED_ERR_OUT"
+    "$F" "$@" >"$ACTUAL_OUT" 2>"$ACTUAL_ERR_OUT"
     output_test "./$BIN" "$@"
 }
 
 # @params: a list of strings
 test_from_direct_input() {
-    bash --posix -c "$@" > "$EXPECTED_OUT" 2> "$EXPECTED_ERR_OUT"
-    "$F" -c "$@" > "$ACTUAL_OUT" 2> "$ACTUAL_ERR_OUT"
+    bash --posix -c "$@" >"$EXPECTED_OUT" 2>"$EXPECTED_ERR_OUT"
+    "$F" -c "$@" >"$ACTUAL_OUT" 2>"$ACTUAL_ERR_OUT"
     output_test "./$BIN -c" "$@"
 }
 
 # @params: err code and then a list of strings
-test_pars_lex_error()
-{
+test_pars_lex_error() {
     ERR="$1"
     shift
     "$F" -c "$@"
@@ -76,9 +74,9 @@ test_pars_lex_error()
 output_test() {
     FA=$1
     shift
-    diff -y --color="always" "$EXPECTED_OUT" "$ACTUAL_OUT" > "$DIFF_OUT" 2>&1
+    diff -y --color="always" "$EXPECTED_OUT" "$ACTUAL_OUT" >"$DIFF_OUT" 2>&1
     if [ $? -eq 0 ]; then
-        diff -y --color="always" "$EXPECTED_ERR_OUT" "$ACTUAL_ERR_OUT" > "$DIFF_ERR" 2>&1
+        diff -y --color="always" "$EXPECTED_ERR_OUT" "$ACTUAL_ERR_OUT" >"$DIFF_ERR" 2>&1
         if [ $? -eq 0 ]; then
             echo "$G[OK]$D $FA \""$@"\""
         else
@@ -90,16 +88,15 @@ output_test() {
             echo "$R$A$D"
         fi
     else
-            echo "$R[KO]$D"
-            echo "$FA \""$@"\""
-            E=$(cat -e "$EXPECTED_OUT")
-            A=$(cat -e "$ACTUAL_OUT")
-            echo "$G$E$D"
-            echo "$R$A$D"
+        echo "$R[KO]$D"
+        echo "$FA \""$@"\""
+        E=$(cat -e "$EXPECTED_OUT")
+        A=$(cat -e "$ACTUAL_OUT")
+        echo "$G$E$D"
+        echo "$R$A$D"
     fi
 }
-test_echo_basic()
-{
+test_echo_basic() {
     echo "========== ECHO BEGIN =========="
     tes "echo     "
     tes 'echo     a'
@@ -112,16 +109,14 @@ test_echo_basic()
     tes "echo foo; echo 'a'"
     echo "========== ECHO END =========="
 }
-test_non_builtin()
-{
+test_non_builtin() {
     echo "========== NON_BUILTIN BEGIN =========="
     tes "ls -a"
     tes "tree -L 2"
     tes "find -name *.c"
     echo "========== NON_BUILTIN END =========="
 }
-test_echo_options()
-{
+test_echo_options() {
     echo "========== ECHO OPTIONS BEGINING =========="
     tes echo -n "just a chill line\n"
     tes echo -e "\n\nhello\nici\n\n\t"
@@ -138,25 +133,22 @@ test_echo_options()
     tes echo -Ee "\tjuuuuuuure\nc\t une \t dinguerie\\\n"
     echo "========== ECHO OPTIONS END =========="
 }
-test_if()
-{
+test_if() {
     echo "========== IF BEGIN =========="
-    tes "if true; then echo a; fi" 
-    tes "if false; then echo a; fi" 
-    tes "if false; then echo a; echo b; fi" 
+    tes "if true; then echo a; fi"
+    tes "if false; then echo a; fi"
+    tes "if false; then echo a; echo b; fi"
     tes "if true; then if true; then if true; then echo a; fi fi fi"
     echo "========== IF END =========="
 }
-test_elif()
-{
+test_elif() {
     echo "========== ELIF BEGIN =========="
     tes "if false; then echo a; elif true; then echo b; fi"
     tes "if false; then echo a; elif false; then echo b;
                         elif true; then echo c; fi"
     echo "========== ELIF END =========="
 }
-test_else()
-{
+test_else() {
     echo "========== ELSE BEGIN =========="
     tes "if false; then echo a; else echo b; fi"
     tes "if true; then echo a; else echo b; fi"
@@ -164,16 +156,14 @@ test_else()
     tes "if false; then echo a; elif false; then echo b; else echo c; fi"
     echo "========== ELSE END =========="
 }
-test_comment()
-{
+test_comment() {
     echo "========== COMMENT BEGIN =========="
     tes echo "Ya un commentaire m c chill #de ouf c chill"
     tes echo "Ya un commentaire m c chill \# mais moi je suis pas un comment"
     tes 'echo \escaped \#escaped "#"quoted not#first #commented'
     echo "========== COMMENT END =========="
 }
-test_mix()
-{
+test_mix() {
     echo "========== MIX BEGIN =========="
     tes 'echo -e \escaped \#escaped "#"quoted not#first #commented'
     echo
@@ -181,8 +171,7 @@ test_mix()
     #commented"; fi'
     echo "========== MIX END =========="
 }
-test_errs()
-{
+test_errs() {
     echo "========== ERROR_CODE BEGIN =========="
     # PARSER ERRS
     test_pars_lex_error 2 "fi fi"
@@ -202,8 +191,7 @@ test_errs()
     test_pars_lex_error 2 "\"\"\""
     echo "========== ERROR_CODE END =========="
 }
-testsuite()
-{
+testsuite() {
     test_echo_basic
     test_echo_options
     test_non_builtin
