f35c35c173c115787c76de7347391b5cd485cb40
Jules Magnan <jules.magnan@epita.fr>
2025-01-16 16:46:44 +0100

    not finished

--- src/ast/nodes/element.c
+++ src/ast/nodes/element.c
@@ -75,13 +75,8 @@ int ast_eval_element(struct ast_element *node, void **out,
         mbt_str_free(str);
         return 0;
     }
-    else if (node->redir)
+    else if (node->redir && ctx->check_redir)
     {
-        if (node->child == 0)
-        {
-            node->child = 1;
-            return 1;
-        }
         ast_eval(node->redir, out, ctx);
         return 1;
     }
