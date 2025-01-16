077724b29fe1ff8dc82e62ec04c618daea9811f6
Jules Magnan <jules.magnan@epita.fr>
2025-01-16 16:59:12 +0100

    fix ionumber done

--- src/ast/nodes/element.c
+++ src/ast/nodes/element.c
@@ -75,8 +75,13 @@ int ast_eval_element(struct ast_element *node, void **out,
         mbt_str_free(str);
         return 0;
     }
-    else if (node->redir && ctx->check_redir)
+    else if (node->redir)
     {
+        if (node->child == 0)
+        {
+            node->child = 1;
+            return 1;
+        }
         ast_eval(node->redir, out, ctx);
         return 1;
     }
