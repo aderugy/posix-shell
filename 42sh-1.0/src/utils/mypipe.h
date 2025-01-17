1eec5467a71fa376724a1c85d045ceaa0904b92f
mehdi.azouz <mehdi.azouz@epita.fr>
2025-01-17 11:15:02 +0100

    add fsanitize

--- 42sh-1.0/src/utils/mypipe.h
+++
@@ -1,6 +0,0 @@
-#ifndef MYPIPE_H
-#define MYPIPE_H
-
-int exec_pipeline(struct linked_list *linked_list);
-
-#endif /* ! MYPIPE_H */
--- 42sh-1.0/src/utils/naming.h
+++
@@ -1,7 +0,0 @@
-#ifndef NAMING_H
-#define NAMING_H
-
-int convention_check(char *name, int len);
-int dollar_valid(int state);
-
-#endif // !NAMING_H
