f058d3d598bfd54cc3a982b6f46be30c08ec565c
gaspard.torterat-slanda <gaspard.torterat-slanda@epita.fr>
2025-01-20 20:18:29 +0100

    fix(command.h): fixed clang-tidy

--- src/builtins/commands.h
+++ src/builtins/commands.h
@@ -3,6 +3,9 @@
 
 #include "builtins.h"
 
+// @Remark; functions were moved to avoid the following clang-tidy:
+// 'file.h containg ten or more function declarations'
+
 int register_commands(void);
 int unregister_commands(void);
 
