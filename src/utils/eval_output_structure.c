cb12b599aa48bf66bf36199e16c260e38199d190
Jules Magnan <jules.magnan@epita.fr>
2025-01-22 19:45:15 +0100

    feat: compile

--- src/utils/eval_output_structure.c
+++ src/utils/eval_output_structure.c
@@ -8,7 +8,7 @@
 struct eval_output *eval_output_init(enum eval_output_type type)
 {
     struct eval_output *eval_output = xcalloc(1, sizeof(struct eval_output));
-    eval_output->eval_output_type = type;
+    eval_output->type = type;
 
     return eval_output;
 }
